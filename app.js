b38f580650a0986d354d48da73d397d15adc0cc6
jduggan131201 <dugganjordan4@gmail.com>
2025-08-27 22:12:17 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const sideMenu = document.getElementById("sideMenu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  sideMenu.classList.toggle("active");
-});
